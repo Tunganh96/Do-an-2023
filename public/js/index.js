b520fa46e01a216869973a228e861a4a1101a425
dev14sinka <106219633+dev14sinka@users.noreply.github.com>
2023-07-26 17:57:51 +0700

    [Component] Add new layout

--- public/js/index.js
+++ public/js/index.js
@@ -30,3 +30,33 @@
 // }
 
 // window.addEventListener("scroll", reveal);
+
+window.addEventListener("scroll", function () {
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+function changeBackground(value) {
+  var listItem = document.querySelectorAll("li.list_item");
+  var bgSection = document.querySelector("#factions")
+  listItem.forEach((item) => {
+    item.classList.remove("active");
+  });
+  if (value === 'necromancer') {
+    var item = document.querySelector(`li.list_item.${value}`)
+    item .classList.add('active')
+  }
+  if (value === 'demon_hunter') {
+    var item = document.querySelector(`li.list_item.${value}`)
+    item .classList.add('active')
+  }
+  if (value === 'mage') {
+    var item = document.querySelector(`li.list_item.${value}`)
+    item .classList.add('active')
+  }
+  if (value === 'barbarian') {
+    var item = document.querySelector(`li.list_item.${value}`)
+    item .classList.add('active')
+  }
+  bgSection.style.background = `center center / cover url('../public/image/${value}.jpg')`
+}
